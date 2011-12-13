d752770db4c02ad2f7f8e6d972743b28b582907a
s <s@s-pc.(none)>
2011-12-14 03:23:26 +0600

    done

--- classes/utilities.php
+++ classes/utilities.php
@@ -7,8 +7,9 @@ if(!class_exists('mysql_utilites')):
 	class mysql_utilites{
 	//db instance
 		function db_object(){
-			$wpdb = new wpdb(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
-			return $wpdb;
+			require_wp_db();
+			wp_set_wpdb_vars();
+			global $wpdb;			
 		}
 		
 		//for mime type
@@ -32,11 +33,88 @@ if(!class_exists('mysql_utilites')):
 		
 		//setting price range
 		function pricerange($price){
-			$price = preg_replace('/^\d/','',$price);
-			$price = (int) $price;
-			global $wpdb;
-			$price_lists = $wpdb->get_col("SELECT ``");
+			$price = preg_replace('/[^0-9]/','',$price);
+			$price = (int) $price;						
+
+			$temrs = get_terms(array('pricerange'));
+			$names = array();
+			$slugs = array();
+			foreach($temrs as $term){
+				$a = utf8_decode($term->name);				
+				$a = preg_replace('/[?]/','-',$a);
+				$a = preg_replace('/[ ]/','',$a);
+				$names[] = $a;
+				$slugs[] = preg_replace('/[^0-9-+]/','',$a);
+				
+			}
+						
+			$plus = array();
+			foreach($slugs as $key=>$slug){
+				$prices = explode('-',$slug);
+				
+				if(count($prices)<2){
+					$plus[$key] = $slug;
+					continue;
+				}
+				
+				if($price >= (int)$prices[0] && $price <= (int)$prices[1]){
+					$found = $key;
+					break;
+				}				
+			}
+			
+			if(isset($found)){
+				return $names[$found];
+			}
+			else{
+				foreach ($plus as $k=>$v){
+					$v = preg_replace('/^\d/','',$v);
+					if($price >= $v){
+						$found = $k;
+						break;
+					}
+				}				
+				return (isset($found)) ? $names[$found] : null;
+			}		
 		}
+
+		//pasring model year from name
+		function model_year($title){
+			preg_match('/\b\d{4,4}\b/',$title,$b);
+			return $b[0];							
+		}
+		
+		//mileage
+		function mileage($mile){
+			//$temrs = get_terms(array('mileage'));
+			$mile = preg_replace('/[^0-9]/','',$mile);
+			$mile .= ' mile';
+			return $mile;		
+		}
+		
+		//audio and video in feature
+		function audio_video($audio){
+			$tax = array();
+			$a = strtolower($audio);
+			if(strstr($a,'am')) $tax[] = 'AM/FM Radio';
+			if(strstr($a,'fm')) $tax[] = 'AM/FM Radio';
+			if(strstr($a,'cd')) $tax[] = 'Compact Disc Player';
+			return $tax;
+		}
+		
+		//meta data
+		function fuel($fuel){
+			if(!$fuel) return;
+						
+			$fuels = array('gasoline', 'diesel', 'flexfuel');
+			if(in_array(strtolower($fuel),$fuels)){
+				return 'Gas';
+			}
+			else{
+				return $fuel;
+			}
+		}
+		
 		
 	}
 	
--- cron/cron.php
+++ cron/cron.php
@@ -3,10 +3,15 @@
  * This script automaticlly run by cron of unix operating system
  * */
 
+set_time_limit(1000);
+
 include '../../../../wp-load.php';
 
 global $wpdb, $cron_utility;
 $cracking_table = $wpdb->prefix . 'cron';
+$max = $wpdb->get_var("SELECT `c_id` FROM $cracking_table ORDER BY `id` DESC LIMIT 1 ");
+if(!max) $max = 0;
+$max = (int) $max;
 
 
 // importing the data
@@ -14,63 +19,100 @@ $info = get_option('cron_information');
 $link = mysql_connect($info['server'], $info['user'], $info['password']);
 mysql_select_db($info['db'], $link);
 $table = $info['table'];
-$query = "SELECT * FROM $table ORDER BY `id` LIMIT 2";
+
+$query = "SELECT * FROM $table WHERE `id`>$max ORDER BY `id`";
 $result = mysql_query($query);
 $cars = array();
 while ($row = mysql_fetch_assoc($result)) {
   $cars[] = $row;
 }
+
 mysql_free_result($result);
 mysql_close($link);
 //closing the database
 
 
-
 //instantiating the wpdb database again
 unset($wpdb);
-$wpdb = $cron_utility->db_object();
+$cron_utility->db_object();
+global $wpdb;
 
 foreach($cars as $car){
-	$equipments = @ unserialize($car['equipment']);
-	$engine = array($cron_utility->engine($car['engine']));
-	$price_range = $cron_utility->pricerange($car['pricerange']);
-	var_dump($price_range);
-	exit;
 	
-	/*
+	// all are the taxonomies
+	$equipments = @ unserialize($car['equipment']);
+	if(!$equipments) $equipments = array();
+ 	$engine = array($cron_utility->engine($car['engine']));
+	$price_range = $cron_utility->pricerange($car['pricerange']);
+	$model_year = $cron_utility->model_year($car['vehicle_name']);	
+	$mileage = $cron_utility->mileage($car['odometer']);
+	$audio = $cron_utility->audio_video($car['audio']);
+	$extras = array($car['interior_type']);
+	$manufacturer = array($car['manufacturer']);
+	$features = array_merge($equipments,$audio,$extras,$manufacturer);
+	
+	
 	$taxanomies = array(
-		'features' => $equipments,
+		'features' => $features,
 		'enginesize' => $engine,
 		'transmission' => array($car['transmission']),
-		'pricerange' => '',
-		'bodytype' => '',
-		'modelyear' => '',
-		'mileage' =>	''	
+		'pricerange' => array($price_range),
+		'bodytype' => array($car['body_type']),
+		'modelyear' => array($model_year),
+		'mileage' => array($mileage)	
 	);
 	
 	$data = array(
 				'post_type' =>'listing',
 				'post_title' => $car['vehicle_name'],
 				'post_content' => isset($car['description']) ? $car['description'] : $car['vehicle_name'],
-				'post_status' => 'draft',
-					
+				'post_status' => 'publish'
+									
 				
 	);
 			//inserting data with some defined data
 	$p_id = wp_insert_post( $data );
 	if(!$p_id) continue;
 	
-	$attachments = unserialize($car['images']);
-	foreach ($attachments as $attachment){
-		$mime = $cron_utility->mime($attachment);
-		$a_data = array(
-				'post_type' =>'attachment',
-				'post_title' => $mime[0],
-				'post_content' => isset($car['description']) ? $car['description'] : $car['vehicle_name'],				
-				'post_status' => 'inherit',
-				'post_mime_type' => 'image/' . $mime[1],
-				'guid' => $attachment
-		);
+	foreach($taxanomies as $taxonmy=>$terms){
+		if(empty($terms)) continue;
+		wp_set_object_terms($p_id,$terms,$taxonmy);
 	}
-	*/
+	
+	
+	$attachments = unserialize($car['images']);
+	if(is_array($attachments)) :
+		foreach ($attachments as $attachment){
+			$mime = $cron_utility->mime($attachment);
+			$a_data = array(
+					'post_type' =>'attachment',
+					'post_title' => $mime[0],
+					'post_content' => isset($car['description']) ? $car['description'] : $car['vehicle_name'],				
+					'post_mime_type' => 'image/' . $mime[1],
+					'guid' => $attachment,
+					'post_parent' => $p_id
+			);
+			wp_insert_attachment($a_data);		
+		}
+	
+	endif;
+	//updating the meta data for the post
+	
+	$fuel = $cron_utility->fuel($car['fuel']);
+	update_post_meta($p_id,'fueltype_value',$fuel);
+	update_post_meta($p_id,'intcolor_value',$car['interior_color']);
+	update_post_meta($p_id,'extcolor_value',$car['exterior_color']);
+	update_post_meta($p_id,'body_type_value',$car['body_type']);
+	update_post_meta($p_id,'door_value',$car['door']);
+	update_post_meta($p_id,'driven_train',$car['drive_train']);
+	update_post_meta($p_id,'trans_value',$car['transmission']);
+	update_post_meta($p_id,'price_value',preg_replace('/[^0-9]/','',$car['price']));
+	update_post_meta($p_id,'mileage_value',preg_replace('/[^0-9]/','',$car['odometer']));
+	update_post_meta($p_id,'top_value',$car['top']);
+	update_post_meta($p_id,'vin_value',$car['vin']);
+	update_post_meta($p_id,'manufacturer_level1_value',$car['manufacturer']);
+	
+	$wpdb->insert($cracking_table,array('p_id'=>(int)$p_id,'c_id'=>(int)$car['id']),array('%d','%d'));
+	
+	
 }
